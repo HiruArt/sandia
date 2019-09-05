40045ea167cd35235f1fb337a65dbd1ea6c76273
DarthJktu <DarthJktu@gmail.com>
2019-09-05 13:55:54 +0300

    dsfsd

--- js/main.js
+++ js/main.js
@@ -22,49 +22,6 @@ $(document).ready(function () {
     });
 
 
-    // $(window).mousemove(function(e) {
-    //     var mauseX = e.pageX;
-    // });
-
-    // $(window).mousemove(function(e) {
-    //     var mauseX = e.pageX;
-    //     var mauseY = e.pageY;
-    //
-    //     hoverT(mauseX, mauseY);
-    // });
-    //
-    //
-    // function hoverT (mauseX, mauseY) {
-    //     $('.triangle__item').each(function (i) {
-    //         $(this).hover(function (){
-    //             var t = $(this);
-    //
-    //             // console.log(mauseX);
-    //
-    //             var tyx = t.offset();
-    //             tx =  Math.floor(tyx.left);
-    //             ty =  Math.floor(tyx.top);
-    //
-    //             console.log(tx);
-    //             console.log(ty);
-    //             // console.log(mauseY, mauseX);
-    //
-    //             if (tyx.left < mauseX)
-    //             {
-    //                 console.log('+');
-    //             }
-    //             else if (tyx.left > mauseX)
-    //             {
-    //                 console.log('-');
-    //             }
-    //             //
-    //             $(this).animate().stop(true);
-    //             // makeNewPosition(pos);
-    //             // animateTriangle(t);
-    //         });
-    //     })
-    // }
-
     let one =  $('.triangle__item--one');
     let two = $('.triangle__item--two');
     let three = $('.triangle__item--three');
@@ -72,74 +29,68 @@ $(document).ready(function () {
     let five = $('.triangle__item--five');
 
 
-    $('.triangle__item').each(function (e) {
+if($(window).width() > 1150 ){
 
-        let mauseXold = 0;
-        let mauseX = 0;
-        let mauseYold = 0;
-        let mauseY = 0;
 
-        setInterval(function () {
-            $(window).mousemove( function (x) {
-                mauseX = x.pageX;
-                mauseY = x.pageY;
-            });
-            mauseXold = mauseX;
-            mauseYold = mauseY;
-        }, 200);
+    var flagHover = false;
+    $('.triangle__item').mousemove(function (e) {
+        if(flagHover == true || $(this).hasClass('triangle__item--four') || $(this).hasClass('triangle__item--five')){
+            return;
+        }
+        flagHover = true;
+        setTimeout(function () {
+            flagHover = false;
+        }, 1000);
+        $(this).animate().stop();
+        var e = e || window.event,
+        x_letter = e.offsetX==undefined?e.layerX:e.offsetX,
+        y_letter = e.offsetY==undefined?e.layerY:e.offsetY,
+        nextCoordX,
+        nextCoordY;
+        var positionXold = $(this).offset().left;
+        var positionYold = $(this).offset().top;
+        var that = $(this);
 
-        let positionXold = $(this).offset().left;
-        let positionYold = $(this).offset().top;
 
-        $(this).hover(
-        function moveRight() {
 
-            let positionX;
-            let positionY;
+        if(x_letter > $(this).width() / 2){
+            nextCoordX = (positionXold + (-x_letter/2)) + 100;
+            nextCoordX = positionXold - (($(this).width() - x_letter) * 2);
 
-            $(this).animate().stop(true);
-            if (mauseX > mauseXold && mauseY < mauseYold ) {
-                positionX = $(this).offset().left + 200;
-                positionY = $(this).offset().top - 200;
-                $(this).animate( { left : positionX, top : positionY}, 1500 );
-                // $(this).animate( { left : +50, top : -50}, 1500 );
-                // $(this).addClass('ttBR');
-            }
-            if (mauseX > mauseXold && mauseY > mauseYold ) {
-                positionX = $(this).offset().left + 200;
-                positionY = $(this).offset().top + 200;
-                $(this).animate( { left : positionX, top : positionY}, 1500 );
-                // $(this).animate( { left : +50, top : +50}, 1500 );
-                // $(this).addClass('ttTR');
+        } else {
+            nextCoordX = (positionXold + x_letter) + 100;
+        }
 
-            }
-            if (mauseX < mauseXold && mauseY < mauseYold ) {
-                positionX = $(this).offset().left - 200;
-                positionY = $(this).offset().top - 200;
-                $(this).animate( { left : positionX, top : positionY}, 1500 );
-                // $(this).animate( { left : -50, top : -50}, 1500 );
-                // $(this).addClass('ttBL');
-            }
-            if (mauseX < mauseXold && mauseY > mauseYold ) {
-                positionX = $(this).offset().left - 200;
-                positionY = $(this).offset().top + 200;
-                $(this).animate( { left : positionX, top : positionY}, 1500 );
-                // $(this).animate( { left : -50, top : +50}, 1500 );
-                // $(this).addClass('ttTL');
-            }
-            console.log($(this));
-        },
-        function () {
+        if(y_letter > $(this).height() / 2){
+            console.log('21312');
+            nextCoordY = positionYold - (($(this).height() - y_letter) * 2);
+        } else {
+            nextCoordY = (y_letter + positionYold) + 100;
+        }
 
-            $(this).animate( { left : positionXold, top : positionYold}, 1500 );
+        console.log('--');
+        console.log(positionYold);
+        console.log(nextCoordY);
+        console.log(y_letter);
 
-            // $(this).removeClass('ttTL');
-            // $(this).removeClass('ttBL');
-            // $(this).removeClass('ttTR');
-            // $(this).removeClass('ttBR');
-            animationStart($(this));
-        })
+        $(this).animate( { left : nextCoordX, top : nextCoordY}, {
+                duration: 3000,
+                specialEasing: {
+                    left: "linear",
+                    top: "linear"
+                }
+            },
+
+            );
+
+
     });
+
+    $('.triangle__item').mouseout(function (e) {
+        animationStart($(this));
+    });
+
+
 
     function animationStart(e) {
         animateTri(e);
@@ -150,12 +101,12 @@ $(document).ready(function () {
     animationStart(three);
     animationStart(four);
     animationStart(five);
-
+}
     $(window).scroll();
 });
 
 function animateTri(e) {
-    let newPos = newPosition();
+    let newPos = newPosition(e);
     let nowPositionElement = e.offset();
     let moveSpeed = calcElementSpeed([nowPositionElement.top, nowPositionElement.left], newPos);
 
@@ -163,22 +114,66 @@ function animateTri(e) {
         animateTri(e);
     });
 }
-function newPosition() {
+function newPosition(el) {
 
-    let h = $('.triangle__inner').height() - 250;
-    let w = $(window).width() - 50;
+    var h = $('.triangle__inner').height() -450;
+    var w = $(window).width() - 250;
 
-    let nextHeightY = Math.floor(Math.random()* h);
-    let nextWidthX = Math.floor(Math.random()* w);
+    var left = el.offset().left;
+    var leftOrRight = (w/2) - left < 0 ? false : true;
+
+
+
+
+    var nextHeightY = Math.floor((Math.random() )* h);
+
+    if($(el).data('top')== '1'){
+        nextHeightY = nextHeightY - 200;
+    } else {
+        nextHeightY = nextHeightY + 300;
+    }
+
+
+    if(leftOrRight == false){
+        var nextWidthX = Math.floor( (w/2) + (Math.random() * 1000) );
+
+        if((nextWidthX - left) > 300 || (nextWidthX - left) < -300){
+
+            nextWidthX = (nextWidthX / 2) + (w/2);
+        }
+
+
+        if(w - nextWidthX > 600){
+            // alert(w - nextWidthX);
+            nextWidthX = nextWidthX + 300;
+            // alert(nextWidthX);
+        }
+
+    } else {
+        var nextWidthX = Math.floor( (Math.random() * 1000) - 300);
+
+        if((nextWidthX - left) > 300 || (nextWidthX - left) < -300){
+
+            nextWidthX = (nextWidthX / 2);
+        }
+
+    }
+
+
+
+
 
     return [nextHeightY, nextWidthX];
 }
 function calcElementSpeed(prev, next) {
 
-    let x = Math.abs(prev[1] - next[1]);
-    let y = Math.abs(prev[0] - next[0]);
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
 
-    let moveSpeed = 5000;
+
+
+    var moveSpeed = Math.random() * (6000 - 4000) + 4000;
+    // console.log(moveSpeed);
 
     return moveSpeed;
 }
